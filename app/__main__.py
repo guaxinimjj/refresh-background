0caf1bf8f9db632264f96c96c74f8b31a2d33953
Vitalii Koval <57917230+guaxinimjj@users.noreply.github.com>
2020-09-29 11:14:36 +0300

    Use random image from unsplash api. (#2)

--- app/__main__.py
+++ app/__main__.py
@@ -1,17 +1,18 @@
 import argparse
-import random
 import time
 import ctypes
 
-from .common import (
-    IMAGES_PATH,
+
+from app.common import (
     SPI_SET_DESKTOP_WALLPAPER,
     SPIF_SEND_WIN_INI_CHANGE,
     SPIF_UPDATE_INI_FILE,
+    save_img,
 )
 
 
 def parse_args() -> argparse.Namespace:
+    """Pars arguments from user or use default args."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i", "--interval", default=10, type=int, help="Refresh interval in minutes."
@@ -19,37 +20,44 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def validate_args(args):
+    """Check."""
+    ...
+
+
+def choose_background() -> str:
+    """Download random image, save to images and return path to the image."""
+    # maximum 50 requests per hour
+    path = save_img()
+    return path
+
+
 def refresh_background(interval):
     while True:
-        set_background()
+        path = choose_background()
+        set_background(path)
         time.sleep(interval)
 
 
-def set_background():
+def set_background(path: str):
     """Change background depending on bit size"""
-    images = list(IMAGES_PATH.iterdir())
-    path = random.choice(images)
-
     ctypes.windll.user32.SystemParametersInfoW(
         SPI_SET_DESKTOP_WALLPAPER,
         0,
-        str(path),
+        path,
         SPIF_SEND_WIN_INI_CHANGE | SPIF_UPDATE_INI_FILE,
     )
 
 
 def main():
+
     args = parse_args()
+    validate_args(args)
     interval = args.interval * 60  # convert minutes to seconds
     try:
         refresh_background(interval)
     except KeyboardInterrupt:
         pass
-    except Exception as err:
-        print("Unhandled exception: {}".format(err))
-        raise
-    else:
-        print('bye bye')
 
 
 if __name__ == "__main__":
--- app/common.py
+++ app/common.py
@@ -1,9 +1,47 @@
 import pathlib
+import os
 
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+UNSPLASH_ACCESS_KEY = os.getenv("UNSPLASH_ACCESS_KEY")
 
 ROOT_PATH = pathlib.Path(__file__).parents[1]
 IMAGES_PATH = ROOT_PATH / "images"
 
+# Check paths
+IMAGES_PATH.mkdir(exist_ok=True)
+
 SPI_SET_DESKTOP_WALLPAPER = 0x0014
 SPIF_UPDATE_INI_FILE = 0x01
 SPIF_SEND_WIN_INI_CHANGE = 0x02
+
+
+def get_img() -> dict:
+    """Download images from Unsplash.com."""
+
+    url = "https://api.unsplash.com/photos/random"
+    params = {}
+    headers = {"Authorization": f"Client-ID {UNSPLASH_ACCESS_KEY}"}
+
+    resp = requests.get(url, params=params, headers=headers)
+    return resp.json()
+
+
+def save_img() -> str:
+    """Save image and return path like str."""
+    body = get_img()
+
+    image_url = body["urls"]["full"]
+    image_id = "{}.jpg".format(body["id"])
+
+    response = requests.get(image_url)
+    content = response.content
+    image_path: pathlib.Path = IMAGES_PATH / image_id
+
+    with open(image_path, "wb") as some_picture:
+        some_picture.write(content)
+
+    return str(image_path)
